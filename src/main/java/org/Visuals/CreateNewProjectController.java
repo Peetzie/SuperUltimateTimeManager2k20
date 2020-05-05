bf4d7907006df9cfa2a84f3dacc5548250e130d5
Frederik Peetz-Schou Larsen <59568959+Peetzie@users.noreply.github.com>
2020-05-05 02:32:52 +0200

    added estimated hours

--- src/main/java/org/Visuals/CreateNewProjectController.java
+++ src/main/java/org/Visuals/CreateNewProjectController.java
@@ -5,17 +5,16 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import org.Backend.Employee;
+import org.Backend.Main;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CreateNewProjectController implements Initializable {
-    ObservableList<String> userList = FXCollections.observableArrayList("User","test","Jackstyle","Do it with u mama");
+    ObservableList<Employee> userList = FXCollections.observableArrayList(Main.getEmployees());
 
     @FXML
     private TextField createProjectTitle;
@@ -37,18 +36,28 @@ public class CreateNewProjectController implements Initializable {
 
     @FXML
     void cancelButtonHandler(ActionEvent event) throws IOException {
-        Launcher.setRoot("adminScreen");
+          Launcher.setRoot("adminScreen");
     }
 
     @FXML
     void createButtonHandler(ActionEvent event) throws IOException {
+        Main.command("newproject " + createProjectTitle.getText() + " " + createProjectDescription.getText().replace(" ", "_") + " " + Integer.parseInt(createProjectEstimatedHours.getText()));
+        if (createProjectProjectManager.getValue() != null){
+            Main.command("assignpm " + (Main.getProjects().size()-1) + " " + Main.getEmployees().indexOf(createProjectProjectManager.getValue()));
+        } else{
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Alert!");
+            alert.setContentText("Creating project with out a project manager");
+            alert.showAndWait();
+        }
         Launcher.setRoot("adminScreen");
 
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        createProjectProjectManager.setValue(userList.get(0));
         createProjectProjectManager.setItems(userList);
     }
+
+
 }
