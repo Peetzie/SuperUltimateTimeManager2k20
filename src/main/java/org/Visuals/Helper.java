c7458cc8232a66767f7e507bbf293975fa655418
ViktorRindom <ViktorRindom@users.noreply.github.com>
2020-05-06 17:35:06 +0200

    kommentar i helper done

--- src/main/java/org/Visuals/Helper.java
+++ src/main/java/org/Visuals/Helper.java
@@ -8,16 +8,16 @@ import javafx.stage.Window;
 
 
 
-public class Helper {
+public class Helper {//class for functions used in a lot of files
 
-    static void closeWindowHelper(ActionEvent event){
+    static void closeWindowHelper(ActionEvent event){//closes the instance of the window
         Window window = ((Node) event.getSource()).getScene().getWindow();
         if (window instanceof Stage){
             ((Stage) window).close();
         }
     }
 
-   static void setText(Label label, String text) {
+   static void setText(Label label, String text) {//simplifies the setText function
         label.setText(text);
     }
 
