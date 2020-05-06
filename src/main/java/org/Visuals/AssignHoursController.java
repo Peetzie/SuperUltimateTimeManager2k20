5a99db46b17ee1e7360df36ae7d078c5b5bcde17
Frederik Peetz-Schou Larsen <59568959+Peetzie@users.noreply.github.com>
2020-05-06 13:01:32 +0200

    Updated AssignHoursController.java and implemented a new helper method for setting label text across different screens

--- src/main/java/org/Visuals/AssignHoursController.java
+++ src/main/java/org/Visuals/AssignHoursController.java
@@ -1,22 +1,30 @@
 package org.Visuals;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import org.Backend.Main;
+import org.Backend.ProjectEmployeeRelation;
 
 import java.io.IOException;
 import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class AssignHoursController implements Initializable {
+    ObservableList<ProjectEmployeeRelation> projectList = FXCollections.observableArrayList(Main.getEmployees().get(Main.getCurrentUser()).getprojectRelations());
+    long startDate;
 
     @FXML
     private Label assignHoursCurrentUser;
 
     @FXML
-    private ChoiceBox assignHoursChooseProject;
+    private ChoiceBox<ProjectEmployeeRelation> assignHoursChooseProject;
 
     @FXML
     private DatePicker assignHoursChooseDate;
@@ -37,7 +45,11 @@ public class AssignHoursController implements Initializable {
     private Button assignHoursCancelButton;
 
     @FXML
-    void assignButtonHandler(ActionEvent event) {
+    void assignButtonHandler(ActionEvent event) throws ParseException {
+        Date date = new SimpleDateFormat("yyyy-MM-dd").parse(assignHoursChooseDate.getValue().toString());
+        startDate = (date.getTime()/1000L);
+        int startTime = Integer.parseInt(assignHoursAssignStartHour.getText())  * 3600 + Integer.parseInt(assignHoursAssignMinute.getText()) * 60;
+        Main.command("assignhours " + assignHoursChooseProject.getValue() + " " + startTime + " " + Math.round(Float.parseFloat(assignHoursDuration.getText())*3600));
 
     }
 
@@ -49,6 +61,8 @@ public class AssignHoursController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println(projectList);
+        assignHoursChooseProject.setItems(projectList);
         Helper.setText(assignHoursCurrentUser,Main.getCurrentUser()+"");
     }
 }
