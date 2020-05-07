1e82b0f409639071f47a7bed748515caf46c2258
Frederik Peetz-Schou Larsen <59568959+Peetzie@users.noreply.github.com>
2020-05-07 22:19:32 +0200

    Added delete a user completion, and added the feature of viewing how many project a user is assigned compared to the total ammounts of projects in the works

--- src/main/java/org/Visuals/adminRemoveEmployeeController.java
+++ src/main/java/org/Visuals/adminRemoveEmployeeController.java
@@ -1,13 +1,18 @@
 package org.Visuals;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
 import org.Backend.Employee;
 import org.Backend.Main;
+import org.Backend.Project;
 
 import java.io.IOException;
 import java.net.URL;
@@ -17,12 +22,14 @@ public class adminRemoveEmployeeController implements Initializable {
     ObservableList<Employee> employees = FXCollections.observableArrayList(Main.getEmployeesReal());
 
 
+
     @FXML
     private ChoiceBox valgbox;
 
     @FXML
-    void ConfirmButtonHandler(ActionEvent event) {
+    void ConfirmButtonHandler(ActionEvent event) throws IOException {
         Main.command("removeemployee " + Main.getEmployees().indexOf(valgbox.getValue()));
+        Launcher.setRoot("adminScreen");
     }
 
     @FXML
