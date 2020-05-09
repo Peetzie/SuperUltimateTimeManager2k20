a0e2c2199a1b197375e707ef25937dab2fd32ee9
Frederik Peetz-Schou Larsen <59568959+Peetzie@users.noreply.github.com>
2020-05-09 20:42:00 +0200

    Added edit hours and Helper funktion for Illigal 24 hour period time period entering

--- src/main/java/org/Visuals/Helper.java
+++ src/main/java/org/Visuals/Helper.java
@@ -3,17 +3,19 @@ package org.Visuals;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.stage.Window;
-
+import org.w3c.dom.Text;
 
 
 public class Helper {//class for functions used in a lot of files
 
-    static void closeWindowHelper(ActionEvent event){//closes the instance of the window
-        Window window = ((Node) event.getSource()).getScene().getWindow();
-        if (window instanceof Stage){
-            ((Stage) window).close();
+    static boolean legalInput(TextField hours, TextField minutes){
+        if(Integer.parseInt(hours.getText()) >= 24 || Integer.parseInt(minutes.getText()) >= 60){
+            return false;
+        } else {
+            return true;
         }
     }
 
