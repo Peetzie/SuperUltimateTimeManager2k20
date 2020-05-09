50a1a6934ec6c2392b62da2b6aabe1b527b36f20
Frederik Peetz-Schou Larsen <59568959+Peetzie@users.noreply.github.com>
2020-05-09 14:20:58 +0200

    Fixed FXML bugs

--- src/main/java/org/Visuals/CreateNewProjectController.java
+++ src/main/java/org/Visuals/CreateNewProjectController.java
@@ -14,10 +14,14 @@ import org.w3c.dom.Text;
 import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.net.URL;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Date;
+import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.zip.DataFormatException;
 
 public class CreateNewProjectController implements Initializable {//controller for create new project scene
     ObservableList<Employee> userList = FXCollections.observableArrayList(Main.getEmployeesReal());
@@ -41,7 +45,6 @@ public class CreateNewProjectController implements Initializable {//controller f
     @FXML
     void createButtonHandler(ActionEvent event) throws IOException, ParseException {//button for sending a create project attempt and takes the admin back to admin interface
         try {
-            int estimatedHours = Integer.parseInt(createProjectEstimatedHours.getText()) * 3600;
             Date date = new SimpleDateFormat("yyyy-MM-dd").parse(createProjectDate.getValue().toString());
             deadline = (date.getTime() / 1000L);
             Main.command("newproject " + createProjectTitle.getText().replace(" ", "_") + " " + createProjectDescription.getText().replace(" ", "_") + " "
@@ -62,6 +65,7 @@ public class CreateNewProjectController implements Initializable {//controller f
             alert.showAndWait();
         }
         }
+
 
     @FXML
     void cancelButtonHandler(ActionEvent event) throws IOException {//takes admin back to admin interface without creating project
