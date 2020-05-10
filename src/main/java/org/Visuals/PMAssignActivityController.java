9009d170ff60359f43be21e39a346de88653b7e9
Frederik Peetz-Schou Larsen <59568959+Peetzie@users.noreply.github.com>
2020-05-10 04:09:28 +0200

    added assign Activity for Viktor

---
+++ src/main/java/org/Visuals/PMAssignActivityController.java
@@ -0,0 +1,16 @@
+package org.Visuals;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
+public class PMAssignActivityController {
+    @FXML
+    private ChoiceBox<?> pmChooseProject;
+
+    @FXML
+    private ChoiceBox<?> pmChooseActivity;
+
+    @FXML
+    private ChoiceBox<?> pmChooseEmployee;
+
+}
--- src/main/java/org/Visuals/ProjectManagerController.java
+++ src/main/java/org/Visuals/ProjectManagerController.java
@@ -23,7 +23,7 @@ public class ProjectManagerController implements Initializable {
             get(Main.getCurrentUser()).getManagerProjects());
     ObservableList<String> projectStatusList = FXCollections.observableArrayList("Not begun yet", "In progress","Finished");
     ObservableList<Employee> employeeList = FXCollections.observableArrayList(Main.getEmployeesReal());
-    ObservableList<String> optionslist = FXCollections.observableArrayList("Create Activities","Remove activity" ,"Edit workhours" +" for other \n project bound employee");
+    ObservableList<String> optionslist = FXCollections.observableArrayList("Create Activities","Remove activity" ,"Assign activity","Edit workhours" +" for other \n project bound employee");
 
     @FXML
     private Label pmCurrentUser;
@@ -104,6 +104,12 @@ public class ProjectManagerController implements Initializable {
                 } else if (pmOtherOptions.getValue().equals("Remove activity")){
                     try {
                         Launcher.setRoot("User/ProjectManager/pmRemoveActivity");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else if(pmOtherOptions.getValue().equals("Assign activity")){
+                    try {
+                        Launcher.setRoot("User/ProjectManager/pmAssignActivity");
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
