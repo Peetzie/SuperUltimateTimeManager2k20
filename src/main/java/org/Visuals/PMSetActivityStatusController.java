9935c3fe01435323e7af8f9a264759555390dea2
ViktorRindom <ViktorRindom@users.noreply.github.com>
2020-05-10 05:27:40 +0200

    vis for set activity status

--- src/main/java/org/Visuals/PMSetActivityStatusController.java
+++ src/main/java/org/Visuals/PMSetActivityStatusController.java
@@ -1,10 +1,23 @@
 package org.Visuals;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
+import org.Backend.Employee;
+import org.Backend.Main;
+import org.Backend.Project;
 
-public class PMSetActivityStatusController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PMSetActivityStatusController implements Initializable {
+    ObservableList<Project> projects = FXCollections.observableArrayList(Main.getProjects());
+    //activity missing
+    ObservableList<String> projectStatusList = FXCollections.observableArrayList("Not begun yet", "In progress","Finished");
 
     @FXML
     private ChoiceBox<?> pmChooseProject;
@@ -16,12 +29,24 @@ public class PMSetActivityStatusController {
     private ChoiceBox<?> pmChooseStatus;
 
     @FXML
-    void cancelButtonHandler(ActionEvent event) {
+    void setButtonHandler(ActionEvent event) {
+        if(pmChooseProject.getValue() !=null && pmChooseActivity.getValue() !=null && ("Not begun yet").equals(pmChooseStatus.getValue())){
+            Main.command("setactivitystatus "+Main.getProjects().indexOf(pmChooseProject.getValue())+" "+Main.getProjects().indexOf(pmChooseActivity.getValue())+" 0");
+        }else if (pmChooseProject.getValue() !=null && ("In progress").equals(pmChooseStatus.getValue())) {
+            Main.command("setactivitystatus "+Main.getProjects().indexOf(pmChooseProject.getValue())+" "+Main.getProjects().indexOf(pmChooseActivity.getValue())+" 1");
+        } else if (pmChooseProject.getValue() !=null && ("Finished").equals(pmChooseStatus.getValue())) {
+            Main.command("setactivitystatus "+Main.getProjects().indexOf(pmChooseProject.getValue())+" "+Main.getProjects().indexOf(pmChooseActivity.getValue())+" 2");
+        }
 
     }
 
     @FXML
-    void setButtonHandler(ActionEvent event) {
+    void cancelButtonHandler(ActionEvent event) throws IOException {
+        Launcher.setRoot("User/ProjectManager/projectManagerScreen");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
 }
