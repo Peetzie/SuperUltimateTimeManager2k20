b52f330d1f8144e4af8f5a709b4bd9c4d57e6c11
ViktorRindom <ViktorRindom@users.noreply.github.com>
2020-05-10 04:17:20 +0200

    vis for remove project

--- src/main/java/org/Visuals/adminRemoveProjectController.java
+++ src/main/java/org/Visuals/adminRemoveProjectController.java
@@ -1,21 +1,37 @@
 package org.Visuals;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
+import org.Backend.Main;
+import org.Backend.Project;
 
-public class adminRemoveProjectController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class adminRemoveProjectController implements Initializable {
+    ObservableList<Project> projects = FXCollections.observableArrayList(Main.getProjects());
 
     @FXML
-    private ChoiceBox<?> removeProject;
+    private ChoiceBox<Project> removeProject;
 
     @FXML
-    void ConfirmButtonHandler(ActionEvent event) {
-
+    void ConfirmButtonHandler(ActionEvent event) throws IOException {
+        Main.command("removeproject "+Main.getProjects().indexOf(removeProject.getValue()));
+        Launcher.setRoot("Admin/adminScreen");
     }
 
     @FXML
-    void exit(ActionEvent event) {
+    void exit(ActionEvent event) throws IOException {
+        Launcher.setRoot("Admin/adminScreen");
+    }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        removeProject.setItems(projects);
     }
 }
