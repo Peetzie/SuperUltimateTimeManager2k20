0773c57ad8f6ecc309bc54c084631bc5f56fc8c9
Frederik Peetz-Schou Larsen <59568959+Peetzie@users.noreply.github.com>
2020-05-10 05:07:01 +0200

    added setup file for activity status

---
+++ src/main/java/org/Visuals/PMSetActivityStatusController.java
@@ -0,0 +1,27 @@
+package org.Visuals;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
+public class PMSetActivityStatusController {
+
+    @FXML
+    private ChoiceBox<?> pmChooseProject;
+
+    @FXML
+    private ChoiceBox<?> pmChooseActivity;
+
+    @FXML
+    private ChoiceBox<?> pmChooseStatus;
+
+    @FXML
+    void cancelButtonHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setButtonHandler(ActionEvent event) {
+
+    }
+}
